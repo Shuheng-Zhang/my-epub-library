c995d62fabf59408f6ac571de108811834ae7572
Shuheng-Zhang <zhangshuheng01@163.com>
2022-04-12 16:12:08 +0800

    修正 代码注释

--- src/main/kotlin/top/shuzz/epub/library/modular/dto/EpubMetaInfoDto.kt
+++ src/main/kotlin/top/shuzz/epub/library/modular/dto/EpubMetaInfoDto.kt
@@ -1,15 +1,34 @@
 package top.shuzz.epub.library.modular.dto
 
 /**
+ * ePub 文件元数据
  * @author heng
  * @since 2022/4/11
  */
 data class EpubMetaInfoDto(
+    /**
+     * 书目标题
+     */
     var bookTitle: String? = null,
+    /**
+     * 书目作者
+     */
     var bookAuthors: String? = null,
+    /**
+     * 书目简介
+     */
     var bookDescription: String? = null,
+    /**
+     * 书目 UUID
+     */
     var epubUid: String? = null,
+    /**
+     * 书目封面URL
+     */
     var coverUrl: String? = null,
+    /**
+     * 书目OPF文件URL
+     */
     var opfUrl: String? = null
 ) {
     override fun toString(): String {
